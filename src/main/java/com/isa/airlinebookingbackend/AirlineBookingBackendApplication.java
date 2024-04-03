3251e039a9a9192682e212296cb8ccb92887d275
Akshayan <takshayan@isa.ae>
2024-04-03 11:11:12 +0530

    Project Initialization

---
+++ src/main/java/com/isa/airlinebookingbackend/AirlineBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.isa.airlinebookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBookingBackendApplication.class, args);
+    }
+
+}
