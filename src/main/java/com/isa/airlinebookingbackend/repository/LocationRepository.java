fd9813612f86a45d49273e0b45679534470469a4
Sunera Jayasinghe <106053595+sunerajayasinghe@users.noreply.github.com>
2024-04-10 16:55:10 +0530

    fix: Item Order Issue after Updating an Entity

--- src/main/java/com/isa/airlinebookingbackend/repository/LocationRepository.java
+++ src/main/java/com/isa/airlinebookingbackend/repository/LocationRepository.java
@@ -4,9 +4,12 @@ import com.isa.airlinebookingbackend.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Optional<Location> findLocationByCode(String code);
+
+    List<Location> findByOrderByIdAsc();
 }
