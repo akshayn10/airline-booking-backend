e00a1ed3a678494b0e94902f503f8f3cbc42686e
Sunera Jayasinghe <106053595+sunerajayasinghe@users.noreply.github.com>
2024-04-10 16:12:27 +0530

    feat: Flight Completion, Cancellation Checking in Fleet View and Status Notification

--- src/main/java/com/isa/airlinebookingbackend/payload/FlightPayload.java
+++ src/main/java/com/isa/airlinebookingbackend/payload/FlightPayload.java
@@ -4,5 +4,8 @@ import java.time.OffsetDateTime;
 
 public record FlightPayload(Long id, Long departureLocation, Long arrivalLocation,
                             OffsetDateTime departureTime, OffsetDateTime arrivalTime,
-                            Long fleetId, Float economyFare, Float premiumFare, Float businessFare) {
+                            Long fleetId, Float economyFare, Float premiumFare, Float businessFare,
+                            Integer remainingEconomySeats, Integer remainingPremiumSeats,
+                            Integer remainingBusinessSeats,
+                            boolean flightCancelled, boolean flightHasBookings) {
 }
